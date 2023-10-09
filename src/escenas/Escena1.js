32b8d274597c40d481145b3039017631d6eb8341
unknown <yaskariri2015@gmail.com>
2023-10-08 21:19:24 -0300

    disparo y puntaje

--- src/escenas/Escena1.js
+++ src/escenas/Escena1.js
@@ -1,11 +1,11 @@
 class Escena1 extends Phaser.Scene {
 
     constructor() {
-        super({key:"Escena1"});
+        super({ key: "Escena1" });
         this.platforms = null;
         this.scoreText = "";
         this.score = 0;
-
+        this.vida = 100;
     }
 
     preload() {
@@ -13,14 +13,21 @@ class Escena1 extends Phaser.Scene {
         this.load.image('enemy', '../public/img/enemy.png');
         this.load.image('red', '../public/img/red.png');
         this.load.image('shoot', '../public/img/shoot.png');
+        this.load.image('disparo', '../public/img/disparo.png')
         this.load.spritesheet('nave', '../public/img/nave.png', { frameWidth: 70, frameHeight: 62 });
-
         this.load.audio('gameMusic', '../public/sound/gamePlay.mp3');
 
     }
 
     create() {
 
+        // crea el disparo 
+        this.input.keyboard.on('keydown', (event) => {
+            if (event.keyCode === 32) {
+
+                this.shoot();  // llama al disparo al apretar la barra espaciadora 
+            }
+        });
 
 
 
@@ -34,32 +41,32 @@ class Escena1 extends Phaser.Scene {
         //--------------------------------------------//
         // this.add.image(400, 300, 'star');    crea una estrella estatica en el escenario 
         this.player = this.physics.add.sprite(100, 100, 'nave');
-        this.player.body.allowGravity=false;
+        this.player.body.allowGravity = false;
         //-------------------------//
-       // this.player.setBounce(0.2);
+        // this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
         //----/
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0}),
+            frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0}),
+            frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'up',
-            frames: this.anims.generateFrameNumbers('nave', { start: 2, end: 2}),
+            frames: this.anims.generateFrameNumbers('nave', { start: 2, end: 2 }),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'down',
-            frames: this.anims.generateFrameNumbers('nave', { start: 1, end: 1}),
+            frames: this.anims.generateFrameNumbers('nave', { start: 1, end: 1 }),
             frameRate: 10,
             repeat: -1
         });
@@ -70,18 +77,19 @@ class Escena1 extends Phaser.Scene {
         this.cursors = this.input.keyboard.createCursorKeys();
 
 
-this.enemys = this.physics.add.group();
+        this.enemys = this.physics.add.group();
 
 
         //Habilita las colisiones de las entrellas con la plataforma
-       // this.physics.add.collider(this.stars, this.platforms);
+        // this.physics.add.collider(this.stars, this.platforms);
 
         //Choque entre las estrellas y el jugador
-       // this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+        // this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
 
         //Para controlar el puntaje
         this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
+        this.vida = this.add.text(16,50,'vida: 100',{fontSize : '32px',fill: '#000'});
         //Para agregar las bombas
         //this.bombs = this.physics.add.group();
         //this.physics.add.collider(this.bombs, this.platforms);
@@ -92,12 +100,12 @@ this.enemys = this.physics.add.group();
     //------------------------//
     update() {
 
-        let particles= this.add.particles(-10,0,'red',{
+        let particles = this.add.particles(-10, 0, 'red', {
 
-            speed:100,
-            angle:{min:150,max:210},
-            scale:{start:1,end:0},
-            blendMode:'ADD'
+            speed: 100,
+            angle: { min: 150, max: 210 },
+            scale: { start: 1, end: 0 },
+            blendMode: 'ADD'
         });
 
         particles.startFollow(this.player);
@@ -127,7 +135,7 @@ this.enemys = this.physics.add.group();
             this.player.anims.play('down', true);
         }
 
-        if (this.score==20){
+        if (this.score == 20) {
             this.gameMusic.destroy();
             this.scene.start('Escena2');
             //this.scene.start('End',{puntaje:this.puntaje}); PARA LLEVAR EL PUNTAJE
@@ -151,9 +159,9 @@ this.enemys = this.physics.add.group();
             bomb.setCollideWorldBounds(true);
             bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
         }
-        
+
     }
-    
+
 
 
     hitBomb(player, bomb) {
@@ -161,6 +169,28 @@ this.enemys = this.physics.add.group();
         player.setTint(0xff0000);
         player.anims.play('turn');
         this.scene.start('FinDelJuego')   // llama a otra escena 
+
+    }
+
+   //colision entre el jugador y el enemigo 
+    hitEnemy(player,enemy){
+        this.vida -=10;
+        this.vida.setText('vida:'+ this.vida);
+    }
+
+
+    // funcion de disparo 
+
+    shoot() {
+
+        let bala = this.physics.add.sprite(this.player.x, this.player.y, 'disparo')
+
+        let velocidadBala = 300;
+        bala.setScale(0.05);
+
+        bala.setVelocity(velocidadBala, 0);
+
+        bala.setCollideWorldBounds(false)
 
     }
 
